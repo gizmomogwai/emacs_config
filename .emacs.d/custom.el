35531953fe44cbccab0a1cccaf7b49831b547e3b
Christian Köstlin <christian.koestlin@gmail.com>
2018-07-22 11:56:51 +0200

    More packages

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,15 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+  '(char-menu
+     (quote
+       ("—" "‘’" "“”" "…" "«»" "–"
+         ("Typography" "•" "©" "†" "‡" "°" "·" "§" "№" "★")
+         ("Math" "≈" "≡" "≠" "∞" "×" "±" "∓" "÷" "√")
+         ("Arrows" "←" "→" "↑" "↓" "⇐" "⇒" "⇑" "⇓")
+         ("Greek" "α" "β" "Y" "δ" "ε" "ζ" "η" "θ" "ι" "κ" "λ" "μ" "ν" "ξ" "ο" "π" "ρ" "σ" "τ" "υ" "φ" "χ" "ψ" "ω"))))
  '(company-minimum-prefix-length 2)
  '(confirm-nonexistent-file-or-buffer nil)
  '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes
-   (quote
-    ("190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" default)))
- '(exec-path
-   (quote
-    ("/Users/gizmo/.cargo/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_10" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_10" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin")))
+  '(custom-safe-themes
+     (quote
+       ("190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" default)))
+  '(exec-path
+     (quote
+       ("/Users/gizmo/.cargo/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_10" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_10" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin")))
+ '(global-whitespace-mode t)
  '(inhibit-startup-screen t)
  '(kill-whole-line t)
  '(lsp-rust-rls-command (quote ("/Users/gizmo/.cargo/bin/rls")))
@@ -20,18 +28,22 @@
  '(ns-antialias-text t)
  '(ns-auto-hide-menu-bar t)
  '(ns-command-modifier (quote meta))
- '(package-selected-packages
-   (quote
-    (diff-hl git-timemachine json-mode plantuml-mode magit-org-todos key-chord dashboard expand-region helm-ag projectile exec-path-from-shell eglot undo-tree color-theme-zenburn lsp-ui-flycheck lsp-flycheck flycheck company editorconfig zenburn-theme helm magit use-package)))
+  '(package-selected-packages
+     (quote
+       (clipmon char-menu typit typeit zzz-to-char ztree aggressive-indent org-htmlslidy minions whole-line-or-region hydra sublimity ecukes diff-hl git-timemachine json-mode plantuml-mode magit-org-todos key-chord expand-region helm-ag projectile exec-path-from-shell eglot undo-tree color-theme-zenburn lsp-ui-flycheck lsp-flycheck flycheck company editorconfig zenburn-theme helm magit use-package)))
  '(plantuml-jar-path "/Users/gizmo/bin/plantuml.1.2018.7.jar")
+ '(projectile-completion-system (quote helm))
+ '(projectile-enable-caching t)
  '(rg-custom-type-aliases nil)
  '(rg-group-result t)
  '(rg-show-columns t)
  '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(whitespace-style (quote (trailing tabs spaces indentation::space tab-mark))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  '(whitespace-hspace ((t nil)))
+  '(whitespace-space ((t nil))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,7 @@
+;;; init.el --- My emacs init script
+;;; Commentary:
+;;; Code:
+
 (message "Running init.el")
 
 (require 'package)
@@ -21,23 +25,41 @@
   (require 'use-package))
 
 (use-package magit
+  :defer 5
+  :ensure t)
+(use-package avy
+  :ensure t)
+(use-package zzz-to-char
+  :ensure t
+  :after (avy)
+  :config (global-set-key (kbd "M-z") #'zzz-to-char))
+
+(use-package whole-line-or-region
+  :ensure t
+  :config (whole-line-or-region-global-mode))
+
+(use-package minions
+  :ensure t
+  :config (minions-mode 1))
+
+(use-package char-menu
   :ensure t)
 
 (use-package key-chord
   :ensure t
   :init (progn
-	  (key-chord-define-global "FF" 'helm-find-files)
-	  (key-chord-define-global "uu" 'undo-tree-visualize)
-	  (key-chord-define-global "xx" 'helm-M-x)
-	  (key-chord-define-global "BB" 'beginning-of-buffer)
-	  (key-chord-define-global "BE" 'end-of-buffer)
-	  (key-chord-define-global "bb" 'helm-mini)
-	  (key-chord-define-global "br" 'kill-buffer)
-	  (key-chord-define-global "bw" 'save-buffer)
-	  (key-chord-define-global "gs" 'magit-status)
-	  (key-chord-define-global "GG" 'goto-line)
-	  (key-chord-define-global "yy" 'helm-show-kill-ring)
-	  (key-chord-mode 1)))
+          (key-chord-define-global "FF" 'helm-find-files)
+          (key-chord-define-global "uu" 'undo-tree-visualize)
+          (key-chord-define-global "xx" 'helm-M-x)
+          (key-chord-define-global "BB" 'beginning-of-buffer)
+          (key-chord-define-global "BE" 'end-of-buffer)
+          (key-chord-define-global "bb" 'helm-mini)
+          (key-chord-define-global "br" 'kill-buffer)
+          (key-chord-define-global "bw" 'save-buffer)
+          (key-chord-define-global "gs" 'magit-status)
+          (key-chord-define-global "GG" 'goto-line)
+          (key-chord-define-global "yy" 'helm-show-kill-ring)
+          (key-chord-mode 1)))
 
 (use-package helm
   :ensure t)
@@ -60,8 +82,8 @@
 
 (use-package flycheck
   :ensure t
-  :config (global-flycheck-mode)
-  :after 'exec-path-from-shell)
+  :config (global-flycheck-mode 1)
+  :after (exec-path-from-shell))
 
 (use-package rust-mode
   :ensure t
@@ -77,7 +99,22 @@
 
 (use-package projectile
   :ensure t
-  :config (projectile-mode))
+  :config (projectile-mode +1))
+
+(defun my-before-switch-project-hook ()
+  "Perform some action after switching Projectile projects."
+  (message "Before switching project...")
+  ;; Do something interesting here...
+  ;;
+  ;; `projectile-current-project-files', and `projectile-current-project-dirs' can be used
+  ;; to get access to the new project's files, and directories.
+  )
+(defun my-after-switch-project-hook ()
+  "Perform some action after switching Projectile projects."
+  (message "Switched project to %s" (projectile-project-root)))
+
+(add-hook 'projectile-before-switch-project-hook #'my-before-switch-project-hook)
+(add-hook 'projectile-after-switch-project-hook #'my-after-switch-project-hook)
 
 (use-package helm-ag
   :ensure t
@@ -95,25 +132,55 @@
   :config (global-auto-revert-mode))
 
 (use-package magit-org-todos
+  :defer 5
   :ensure t
   :config (magit-org-todos-autoinsert))
 
 (use-package plantuml-mode
+  :defer 5
   :ensure t
   :config (plantuml-set-output-type "utxt"))
 
 (use-package json-mode
+  :defer 5
   :ensure t)
 
 (use-package git-timemachine
+  :defer 10
   :ensure t)
 
 (use-package diff-hl
+  :defer 5
   :ensure t
-  :config (global-diff-hl-mode))
+  :config (global-diff-hl-mode 1))
+
+(use-package ecukes
+  :defer 10
+  :ensure t)
+
+(use-package sublimity
+  :ensure t
+  :config
+  (require 'sublimity)
+  (require 'sublimity-map)
+  (sublimity-map-set-delay 0)
+  (sublimity-mode 1)
+  )
+
+(use-package aggressive-indent
+  :ensure t
+  :config (global-aggressive-indent-mode 1))
+
+(use-package org-htmlslidy
+  :ensure t)
+(use-package ztree
+  :ensure t)
+
+(use-package clipmon
+  :ensure t
+  :config (clipmon-mode-start))
 
 (setq-default header-line-format '(:eval (which-function)))
-
 
 (defun smarter-move-beginning-of-line (arg)
   "Move point back to indentation of beginning of line.
@@ -144,3 +211,5 @@ point reaches the beginning or end of the buffer, stop there."
 (if (< (length command-line-args) 2 )
     (helm-recentf))
 
+(provide 'init)
+;;; init.el ends here
