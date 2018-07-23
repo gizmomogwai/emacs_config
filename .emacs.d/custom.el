c58452860c25190bb15a060af5d8d8672b2fc195
Christian Köstlin <christian.koestlin@gmail.com>
2018-07-23 17:15:17 +0200

    Migrate to straight

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,22 +3,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  '(char-menu
-     (quote
-       ("—" "‘’" "“”" "…" "«»" "–"
-         ("Typography" "•" "©" "†" "‡" "°" "·" "§" "№" "★")
-         ("Math" "≈" "≡" "≠" "∞" "×" "±" "∓" "÷" "√")
-         ("Arrows" "←" "→" "↑" "↓" "⇐" "⇒" "⇑" "⇓")
-         ("Greek" "α" "β" "Y" "δ" "ε" "ζ" "η" "θ" "ι" "κ" "λ" "μ" "ν" "ξ" "ο" "π" "ρ" "σ" "τ" "υ" "φ" "χ" "ψ" "ω"))))
+ '(char-menu
+   (quote
+    ("—" "‘’" "“”" "…" "«»" "–"
+     ("Typography" "•" "©" "†" "‡" "°" "·" "§" "№" "★")
+     ("Math" "≈" "≡" "≠" "∞" "×" "±" "∓" "÷" "√")
+     ("Arrows" "←" "→" "↑" "↓" "⇐" "⇒" "⇑" "⇓")
+     ("Greek" "α" "β" "Y" "δ" "ε" "ζ" "η" "θ" "ι" "κ" "λ" "μ" "ν" "ξ" "ο" "π" "ρ" "σ" "τ" "υ" "φ" "χ" "ψ" "ω"))))
  '(company-minimum-prefix-length 2)
  '(confirm-nonexistent-file-or-buffer nil)
  '(custom-enabled-themes (quote (zenburn)))
-  '(custom-safe-themes
-     (quote
-       ("190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" default)))
-  '(exec-path
-     (quote
-       ("/Users/gizmo/.cargo/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_10" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_10" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin")))
+ '(custom-safe-themes
+   (quote
+    ("420459d6eeb45aadf5db5fbcc3d6990b65141c104911f7359454fc29fa9d87a0" "190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" default)))
+ '(exec-path
+   (quote
+    ("/Users/gizmo/.cargo/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_10" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_10" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin")))
  '(global-whitespace-mode t)
  '(inhibit-startup-screen t)
  '(kill-whole-line t)
@@ -28,9 +28,9 @@
  '(ns-antialias-text t)
  '(ns-auto-hide-menu-bar t)
  '(ns-command-modifier (quote meta))
-  '(package-selected-packages
-     (quote
-       (clipmon char-menu typit typeit zzz-to-char ztree aggressive-indent org-htmlslidy minions whole-line-or-region hydra sublimity ecukes diff-hl git-timemachine json-mode plantuml-mode magit-org-todos key-chord expand-region helm-ag projectile exec-path-from-shell eglot undo-tree color-theme-zenburn lsp-ui-flycheck lsp-flycheck flycheck company editorconfig zenburn-theme helm magit use-package)))
+ '(package-selected-packages
+   (quote
+    (clipmon char-menu typit typeit zzz-to-char ztree aggressive-indent org-htmlslidy minions whole-line-or-region hydra sublimity ecukes diff-hl git-timemachine json-mode plantuml-mode magit-org-todos key-chord expand-region helm-ag projectile exec-path-from-shell eglot undo-tree color-theme-zenburn lsp-ui-flycheck lsp-flycheck flycheck company editorconfig zenburn-theme helm magit use-package)))
  '(plantuml-jar-path "/Users/gizmo/bin/plantuml.1.2018.7.jar")
  '(projectile-completion-system (quote helm))
  '(projectile-enable-caching t)
@@ -41,9 +41,9 @@
  '(tool-bar-mode nil)
  '(whitespace-style (quote (trailing tabs spaces indentation::space tab-mark))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(whitespace-hspace ((t nil)))
-  '(whitespace-space ((t nil))))
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-hspace ((t nil)))
+ '(whitespace-space ((t nil))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,188 +2,140 @@
 ;;; Commentary:
 ;;; Code:
 
-(message "Running init.el")
+(defvar bootstrap-version)
+(let ((bootstrap-file
+        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+       (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+      (url-retrieve-synchronously
+        "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+        'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(require 'package)
 (defalias 'yes-or-no-p 'y-or-n-p)
-
 (add-to-list 'initial-frame-alist '(font . "Fira Code Retina-15"))
 (add-to-list 'default-frame-alist '(font . "Fira Code Retina-15"))
-
 (toggle-frame-maximized)
+(setq-default header-line-format '(:eval (which-function)))
 
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-(package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(eval-when-compile
-  (require 'use-package))
+(straight-use-package 'use-package)
 
 (use-package magit
-  :defer 5
-  :ensure t)
-(use-package avy
-  :ensure t)
-(use-package zzz-to-char
-  :ensure t
-  :after (avy)
-  :config (global-set-key (kbd "M-z") #'zzz-to-char))
-
-(use-package whole-line-or-region
-  :ensure t
-  :config (whole-line-or-region-global-mode))
-
-(use-package minions
-  :ensure t
-  :config (minions-mode 1))
-
-(use-package char-menu
-  :ensure t)
-
-(use-package key-chord
-  :ensure t
-  :init (progn
-          (key-chord-define-global "FF" 'helm-find-files)
-          (key-chord-define-global "uu" 'undo-tree-visualize)
-          (key-chord-define-global "xx" 'helm-M-x)
-          (key-chord-define-global "BB" 'beginning-of-buffer)
-          (key-chord-define-global "BE" 'end-of-buffer)
-          (key-chord-define-global "bb" 'helm-mini)
-          (key-chord-define-global "br" 'kill-buffer)
-          (key-chord-define-global "bw" 'save-buffer)
-          (key-chord-define-global "gs" 'magit-status)
-          (key-chord-define-global "GG" 'goto-line)
-          (key-chord-define-global "yy" 'helm-show-kill-ring)
-          (key-chord-mode 1)))
-
-(use-package helm
-  :ensure t)
+  :straight t)
 
 (use-package zenburn-theme
-  :ensure t
+  :straight t
   :config (load-theme 'zenburn t))
 
+(use-package whole-line-or-region
+  :straight t
+  :config (whole-line-or-region-global-mode))
+
+(use-package org-kanban
+  :straight t)
 (use-package editorconfig
-  :ensure t
+  :straight t
   :config (editorconfig-mode 1))
 
+(use-package minions
+  :straight t
+  :config (minions-mode 1))
+
+(use-package key-chord
+  :straight t
+  :config
+  (key-chord-define-global "FF" 'helm-find-files)
+  (key-chord-define-global "uu" 'undo-tree-visualize)
+  (key-chord-define-global "xx" 'helm-M-x)
+  (key-chord-define-global "BB" 'beginning-of-buffer)
+  (key-chord-define-global "BE" 'end-of-buffer)
+  (key-chord-define-global "bb" 'helm-mini)
+  (key-chord-define-global "br" 'kill-buffer)
+  (key-chord-define-global "bw" 'save-buffer)
+  (key-chord-define-global "gs" 'magit-status)
+  (key-chord-define-global "GG" 'goto-line)
+  (key-chord-define-global "yy" 'helm-show-kill-ring)
+  (key-chord-mode 1))
+
+(use-package helm
+  :straight t)
+
+(use-package char-menu
+  :straight t)
+
+(use-package undo-tree
+  :straight t)
+
+(use-package projectile
+  :straight t
+  :config (projectile-mode 1))
+
 (use-package company
-  :ensure t
-  :config (global-company-mode))
+  :straight t
+  :config (global-company-mode 1))
 
 (use-package exec-path-from-shell
-  :ensure t
+  :straight t
   :config (exec-path-from-shell-initialize))
 
 (use-package flycheck
-  :ensure t
+  :straight t
   :config (global-flycheck-mode 1)
   :after (exec-path-from-shell))
 
-(use-package rust-mode
-  :ensure t
-  :mode "\\.rs\\'"
-  :init (setq rust-format-on-save t))
-
-(use-package eglot
-  :ensure t
-  :init (add-hook 'rust-mode-hook 'eglot-ensure))
-
-(use-package undo-tree
-  :ensure t)
-
-(use-package projectile
-  :ensure t
-  :config (projectile-mode +1))
-
-(defun my-before-switch-project-hook ()
-  "Perform some action after switching Projectile projects."
-  (message "Before switching project...")
-  ;; Do something interesting here...
-  ;;
-  ;; `projectile-current-project-files', and `projectile-current-project-dirs' can be used
-  ;; to get access to the new project's files, and directories.
-  )
-(defun my-after-switch-project-hook ()
-  "Perform some action after switching Projectile projects."
-  (message "Switched project to %s" (projectile-project-root)))
-
-(add-hook 'projectile-before-switch-project-hook #'my-before-switch-project-hook)
-(add-hook 'projectile-after-switch-project-hook #'my-after-switch-project-hook)
-
 (use-package helm-ag
-  :ensure t
+  :straight t
   :config (global-set-key (kbd "M-s") 'helm-ag-project-root))
 
 (use-package expand-region
-  :ensure t
+  :straight t
   :config (global-set-key (kbd "C-M-w") 'er/expand-region))
 
-(use-package org
-  :ensure t)
-
-(use-package autorevert
-  :ensure t
-  :config (global-auto-revert-mode))
+(use-package multiple-cursors
+  :straight t
+  :config
+  (global-set-key (kbd "M-n") 'mc/mark-next-like-this)
+  (global-set-key (kbd "M-p") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "M-a") 'mc/mark-all-like-this))
 
 (use-package magit-org-todos
-  :defer 5
-  :ensure t
+  :straight t
   :config (magit-org-todos-autoinsert))
 
 (use-package plantuml-mode
-  :defer 5
-  :ensure t
+  :straight t
   :config (plantuml-set-output-type "utxt"))
 
 (use-package json-mode
-  :defer 5
-  :ensure t)
+  :straight t)
 
 (use-package git-timemachine
-  :defer 10
-  :ensure t)
+  :straight t)
 
 (use-package diff-hl
-  :defer 5
-  :ensure t
+  :straight t
   :config (global-diff-hl-mode 1))
 
 (use-package ecukes
-  :defer 10
-  :ensure t)
-
-(use-package sublimity
-  :ensure t
-  :config
-  (require 'sublimity)
-  (require 'sublimity-map)
-  (sublimity-map-set-delay 0)
-  (sublimity-mode 1)
-  )
+  :straight t)
 
 (use-package aggressive-indent
-  :ensure t
+  :straight t
   :config (global-aggressive-indent-mode 1))
 
-(use-package org-htmlslidy
-  :ensure t)
-(use-package ztree
-  :ensure t)
+(use-package autorevert
+  :straight t
+  :config (global-auto-revert-mode))
 
-(use-package clipmon
-  :ensure t
-  :config (clipmon-mode-start))
-
-(setq-default header-line-format '(:eval (which-function)))
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 (defun smarter-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
+  "Move point back to indentation or beginning of line.
 Move point to the first non-whitespace character on this line.
 If point is already there, move to the beginning of the line.
 Effectively toggle between the first non-whitespace character and
@@ -193,7 +145,6 @@ point reaches the beginning or end of the buffer, stop there."
   (interactive "^p")
   (setq arg (or arg 1))
 
-  ;; Move lines first
   (when (/= arg 1)
     (let ((line-move-visual nil))
       (forward-line (1- arg))))
@@ -203,13 +154,43 @@ point reaches the beginning or end of the buffer, stop there."
     (when (= orig-point (point))
       (move-beginning-of-line 1))))
 
-(global-set-key [remap move-beginning-of-line]
-                'smarter-move-beginning-of-line)
+(global-set-key [remap move-beginning-of-line] 'smarter-move-beginning-of-line)
 
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-(if (< (length command-line-args) 2 )
-    (helm-recentf))
+(if (< (length command-line-args) 2)
+  (helm-recentf))
 
 (provide 'init)
+
 ;;; init.el ends here
+
+;;(use-package avy
+;;  :ensure t)
+;;(use-package zzz-to-char
+;;  :ensure t
+;;  :after (avy)
+;;  :config (global-set-key (kbd "M-z") #'zzz-to-char))
+;;
+;;(use-package rust-mode
+;;  :ensure t
+;;  :mode "\\.rs\\'"
+;;  :init (setq rust-format-on-save t))
+;;
+;;(use-package eglot
+;;  :ensure t
+;;  :init (add-hook 'rust-mode-hook 'eglot-ensure))
+;;
+;;(defun my-before-switch-project-hook ()
+;;  "Perform some action after switching Projectile projects."
+;;  (message "Before switching project...")
+;;  ;; Do something interesting here...
+;;  ;;
+;;  ;; `projectile-current-project-files', and `projectile-current-project-dirs' can be used
+;;  ;; to get access to the new project's files, and directories.
+;;  )
+;;(defun my-after-switch-project-hook ()
+;;  "Perform some action after switching Projectile projects."
+;;  (message "Switched project to %s" (projectile-project-root)))
+;;
+;;(add-hook 'projectile-before-switch-project-hook #'my-before-switch-project-hook)
+;;(add-hook 'projectile-after-switch-project-hook #'my-after-switch-project-hook)
+
