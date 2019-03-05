9cdaa7f839be3c31078d98fa3e3e2e6001feb985
Christian Koestlin <christian.koestlin@esrlabs.com>
2019-03-05 07:24:06 +0100

    More config
    Ticket:

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,47 +3,177 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(char-menu
-   (quote
-    ("—" "‘’" "“”" "…" "«»" "–"
-     ("Typography" "•" "©" "†" "‡" "°" "·" "§" "№" "★")
-     ("Math" "≈" "≡" "≠" "∞" "×" "±" "∓" "÷" "√")
-     ("Arrows" "←" "→" "↑" "↓" "⇐" "⇒" "⇑" "⇓")
-     ("Greek" "α" "β" "Y" "δ" "ε" "ζ" "η" "θ" "ι" "κ" "λ" "μ" "ν" "ξ" "ο" "π" "ρ" "σ" "τ" "υ" "φ" "χ" "ψ" "ω"))))
+ '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d.autosave/" t))))
+ '(auto-save-list-file-prefix "~/.emacs.d.autosave/")
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.d.backup/"))))
+ '(c-offsets-alist (quote ((substatement-open . 0))))
+ '(calendar-date-style (quote iso))
+ '(calendar-intermonth-header "KW")
+  '(calendar-intermonth-text
+     (quote
+       (propertize
+         (format "%2d"
+           (car
+             (calendar-iso-from-absolute
+               (calendar-absolute-from-gregorian
+                 (list month day year)))))
+         (quote font-lock-face)
+         (quote calendar-iso-week-face))))
+ '(calendar-today-marker (quote calendar-today))
+ '(calendar-today-visible-hook (quote (calendar-mark-today)))
+  '(char-menu
+     (quote
+       ("—" "‘’" "“”" "…" "«»" "–"
+         ("Typography" "•" "©" "†" "‡" "°" "·" "§" "№" "★")
+         ("Math" "≈" "≡" "≠" "∞" "×" "±" "∓" "÷" "√")
+         ("Arrows" "←" "→" "↑" "↓" "⇐" "⇒" "⇑" "⇓")
+         ("Greek" "α" "β" "Y" "δ" "ε" "ζ" "η" "θ" "ι" "κ" "λ" "μ" "ν" "ξ" "ο" "π" "ρ" "σ" "τ" "υ" "φ" "χ" "ψ" "ω"))))
+ '(company-dabbrev-downcase nil)
+  '(company-frontends
+     (quote
+       (company-pseudo-tooltip-unless-just-one-frontend company-preview-frontend company-echo-metadata-frontend)))
  '(company-minimum-prefix-length 2)
+ '(compilation-scroll-output (quote first-error))
  '(confirm-nonexistent-file-or-buffer nil)
  '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes
-   (quote
-    ("420459d6eeb45aadf5db5fbcc3d6990b65141c104911f7359454fc29fa9d87a0" "190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" default)))
- '(exec-path
-   (quote
-    ("/Users/gizmo/.cargo/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_10" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_10" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin")))
+  '(custom-safe-themes
+     (quote
+       ("420459d6eeb45aadf5db5fbcc3d6990b65141c104911f7359454fc29fa9d87a0" "190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" default)))
+ '(debug-on-error nil)
+ '(delete-old-versions t)
+  '(editorconfig-indentation-alist
+     (quote
+       ((apache-mode apache-indent-level)
+         (awk-mode c-basic-offset)
+         (c++-mode c-basic-offset)
+         (d-mode c-basic-offset)
+         (c-mode c-basic-offset)
+         (cmake-mode cmake-tab-width)
+         (coffee-mode coffee-tab-width)
+         (cperl-mode cperl-indent-level)
+         (crystal-mode crystal-indent-level)
+         (csharp-mode c-basic-offset)
+         (css-mode css-indent-offset)
+         (emacs-lisp-mode lisp-indent-offset)
+         (erlang-mode erlang-indent-level)
+         (ess-mode ess-indent-offset)
+         (feature-mode feature-indent-offset feature-indent-level)
+         (fsharp-mode fsharp-continuation-offset fsharp-indent-level fsharp-indent-offset)
+         (groovy-mode groovy-indent-offset)
+         (haskell-mode haskell-indent-spaces haskell-indent-offset haskell-indentation-layout-offset haskell-indentation-left-offset haskell-indentation-starter-offset haskell-indentation-where-post-offset haskell-indentation-where-pre-offset shm-indent-spaces)
+         (idl-mode c-basic-offset)
+         (jade-mode jade-tab-width)
+         (java-mode c-basic-offset)
+         (js-mode js-indent-level)
+         (js-jsx-mode js-indent-level sgml-basic-offset)
+         (js2-mode js2-basic-offset)
+         (js2-jsx-mode js2-basic-offset sgml-basic-offset)
+         (js3-mode js3-indent-level)
+         (json-mode js-indent-level)
+         (julia-mode julia-indent-offset)
+         (latex-mode . editorconfig-set-indentation/latex-mode)
+         (lisp-mode lisp-indent-offset)
+         (livescript-mode livescript-tab-width)
+         (lua-mode lua-indent-level)
+         (matlab-mode matlab-indent-level)
+         (mustache-mode mustache-basic-offset)
+         (nginx-mode nginx-indent-level)
+         (nxml-mode nxml-child-indent
+           (nxml-attribute-indent . 2))
+         (objc-mode c-basic-offset)
+         (octave-mode octave-block-offset)
+         (perl-mode perl-indent-level)
+         (php-mode c-basic-offset)
+         (pike-mode c-basic-offset)
+         (ps-mode ps-mode-tab)
+         (pug-mode pug-tab-width)
+         (puppet-mode puppet-indent-level)
+         (python-mode . editorconfig-set-indentation/python-mode)
+         (ruby-mode ruby-indent-level)
+         (rust-mode rust-indent-offset)
+         (scala-mode scala-indent:step)
+         (scss-mode css-indent-offset)
+         (sgml-mode sgml-basic-offset)
+         (sh-mode sh-basic-offset sh-indentation)
+         (slim-mode slim-indent-offset)
+         (tcl-mode tcl-indent-level tcl-continued-indent-level)
+         (typescript-mode typescript-indent-level)
+         (verilog-mode verilog-indent-level verilog-indent-level-behavioral verilog-indent-level-declaration verilog-indent-level-module verilog-cexp-indent verilog-case-indent)
+         (web-mode
+           (web-mode-indent-style lambda
+             (size)
+             2)
+           web-mode-attr-indent-offset web-mode-attr-value-indent-offset web-mode-code-indent-offset web-mode-css-indent-offset web-mode-markup-indent-offset web-mode-sql-indent-offset web-mode-block-padding web-mode-script-padding web-mode-style-padding)
+         (yaml-mode yaml-indent-offset))))
+  '(exec-path
+     (quote
+       ("/Users/gizmo/.cargo/bin" "/usr/local/bin" "/usr/bin" "/bin" "/usr/sbin" "/sbin" "/Applications/Emacs.app/Contents/MacOS/bin-x86_64-10_10" "/Applications/Emacs.app/Contents/MacOS/libexec-x86_64-10_10" "/Applications/Emacs.app/Contents/MacOS/libexec" "/Applications/Emacs.app/Contents/MacOS/bin")))
+ '(gcal-mode-calendar (quote christian\.koestlin))
+ '(gcal-mode-days-after 3)
+ '(gcalcli-mode-calendar "christian.koestlin")
+ '(gcalcli-mode-days-to-show 3)
+ '(global-display-line-numbers-mode t)
  '(global-whitespace-mode t)
+ '(helm-mode-fuzzy-match t)
  '(inhibit-startup-screen t)
  '(kill-whole-line t)
  '(lsp-rust-rls-command (quote ("/Users/gizmo/.cargo/bin/rls")))
+ '(markdown-command "pandoc")
  '(menu-bar-mode nil)
  '(ns-alternate-modifier (quote none))
  '(ns-antialias-text t)
  '(ns-auto-hide-menu-bar t)
  '(ns-command-modifier (quote meta))
- '(package-selected-packages
-   (quote
-    (clipmon char-menu typit typeit zzz-to-char ztree aggressive-indent org-htmlslidy minions whole-line-or-region hydra sublimity ecukes diff-hl git-timemachine json-mode plantuml-mode magit-org-todos key-chord expand-region helm-ag projectile exec-path-from-shell eglot undo-tree color-theme-zenburn lsp-ui-flycheck lsp-flycheck flycheck company editorconfig zenburn-theme helm magit use-package)))
+ '(org-agenda-files (quote ("~/Dropbox/org/")))
+ '(org-closed-keep-when-no-todo t)
+ '(org-confirm-babel-evaluate nil)
+ '(org-log-done (quote note))
+ '(org-plantuml-jar-path "~/bin/plantuml.1.2018.13.jar")
+  '(package-selected-packages
+     (quote
+       (clipmon char-menu typit typeit zzz-to-char ztree aggressive-indent org-htmlslidy minions whole-line-or-region hydra sublimity ecukes diff-hl git-timemachine json-mode plantuml-mode magit-org-todos key-chord expand-region helm-ag projectile exec-path-from-shell eglot undo-tree color-theme-zenburn lsp-ui-flycheck lsp-flycheck flycheck company editorconfig zenburn-theme helm magit use-package)))
  '(plantuml-jar-path "/Users/gizmo/bin/plantuml.1.2018.7.jar")
  '(projectile-completion-system (quote helm))
  '(projectile-enable-caching t)
+ '(projectile-indexing-method (quote hybrid))
+ '(projectile-require-project-root nil)
+ '(recenter-positions (quote (top middle bottom)))
  '(rg-custom-type-aliases nil)
  '(rg-group-result t)
  '(rg-show-columns t)
+  '(safe-local-variable-values
+     (quote
+       ((eval org-columns)
+         (eval goto-char
+           (point-min))
+         (checkdoc-package-keywords-flag)
+         (eval progn
+           (c-set-offset
+             (quote arglist-intro)
+             (quote 2))
+           (c-set-offset
+             (quote arglist-close)
+             (quote 0)))
+         (org-duration-format . h:mm)
+         (org-tags-column . 120)
+         (org-tags-column . 100)
+         (org-tags-column . 140)
+         (org-tags-column . 90)
+         (org-log-done))))
  '(scroll-bar-mode nil)
+ '(scroll-conservatively 10000)
+ '(scroll-preserve-screen-position t)
  '(tool-bar-mode nil)
- '(whitespace-style (quote (trailing tabs spaces indentation::space tab-mark))))
+ '(version-control t)
+ '(view-read-only t)
+ '(whitespace-style (quote (trailing tabs spaces indentation::space tab-mark)))
+ '(x-stretch-cursor t))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(whitespace-hspace ((t nil)))
- '(whitespace-space ((t nil))))
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  '(calendar-iso-week-face ((t (:foreground "gray40"))) t)
+  '(calendar-today ((t (:inverse-video t))))
+  '(whitespace-hspace ((t nil)))
+  '(whitespace-space ((t nil))))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,6 @@
-;;; init.el --- My emacs init script
+;;; init.el --- My emacs init script -*- lexical-binding: t -*-
 ;;; Commentary:
+;;; https://github.com/wasamasa/dotemacs/blob/master/init.org
 ;;; Code:
 
 (defvar bootstrap-version)
@@ -16,15 +17,36 @@
   (load bootstrap-file nil 'nomessage))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
-(add-to-list 'initial-frame-alist '(font . "Fira Code Retina-15"))
-(add-to-list 'default-frame-alist '(font . "Fira Code Retina-15"))
-(toggle-frame-maximized)
+                                        ;(add-to-list 'initial-frame-alist '(font . "Fira Code Retina-15"))
+                                        ;(add-to-list 'default-frame-alist '(font . "Fira Code Retina-15"))
+                                        ;(add-to-list 'initial-frame-alist '(font . "Hack-15"))
+                                        ;(add-to-list 'default-frame-alist '(font . "Hack-15"))
+(add-to-list 'initial-frame-alist '(font . "Iosevka-15"))
+(add-to-list 'default-frame-alist '(font . "Iosevka-15"))
+                                        ;(add-to-list 'initial-frame-alist '(font . "Envy Code R-15"))
+                                        ;(add-to-list 'default-frame-alist '(font . "Envy Code R-15"))
+                                        ;(toggle-frame-maximized)
 (setq-default header-line-format '(:eval (which-function)))
-
 
 (straight-use-package 'use-package)
 
 (use-package magit
+  :straight t
+  :config
+  (magit-define-popup-action 'magit-push-popup
+    ?g
+    "Push to gerrit"
+    'magit-push-to-gerrit)
+  (defun magit-push-to-gerrit (remote target)
+    "Push to remote REMOTE to refs/for/TARGET."
+    (interactive
+      (let* (
+              (remote (magit-read-remote "Remote" "origin"))
+              (target (magit-read-remote-branch "Target branch" remote "master" nil)))
+        (list remote target)))
+    (magit-run-git-async "push" remote (format "HEAD:refs/for/%s" target))))
+
+(use-package git-timemachine
   :straight t)
 
 (use-package zenburn-theme
@@ -35,8 +57,18 @@
   :straight t
   :config (whole-line-or-region-global-mode))
 
-(use-package org-kanban
+(use-package ranger
   :straight t)
+
+;;(use-package org-kanban
+;;:straight t)
+
+(use-package org-pomodoro
+  :straight t)
+(use-package magit-org-todos
+  :straight t
+  :config (magit-org-todos-autoinsert))
+
 (use-package editorconfig
   :straight t
   :config (editorconfig-mode 1))
@@ -45,10 +77,26 @@
   :straight t
   :config (minions-mode 1))
 
+(defun my-d-mode-setup ()
+  "Setup indent according to dfmt's defaults."
+  (add-to-list 'c-offsets-alist
+    '(substatement-open . 0)))
+
+(use-package d-mode
+  :straight t
+  :config (add-hook 'd-mode-hook 'my-d-mode-setup))
+
+(use-package helpful
+  :straight t)
+
+(use-package docker
+  :straight t)
+(use-package dockerfile-mode
+  :straight t)
 (use-package key-chord
   :straight t
   :config
-  (key-chord-define-global "FF" 'helm-find-files)
+  (key-chord-define-global "FF" 'helm-projectile)
   (key-chord-define-global "uu" 'undo-tree-visualize)
   (key-chord-define-global "xx" 'helm-M-x)
   (key-chord-define-global "BB" 'beginning-of-buffer)
@@ -62,7 +110,8 @@
   (key-chord-mode 1))
 
 (use-package helm
-  :straight t)
+  :straight t
+  :config (helm-mode +1))
 
 (use-package char-menu
   :straight t)
@@ -73,6 +122,12 @@
 (use-package projectile
   :straight t
   :config (projectile-mode 1))
+(use-package projectile-rails
+  :straight t
+  :config (projectile-rails-global-mode 1))
+
+(use-package helm-projectile
+  :straight t)
 
 (use-package company
   :straight t
@@ -81,6 +136,9 @@
 (use-package exec-path-from-shell
   :straight t
   :config (exec-path-from-shell-initialize))
+
+(use-package yaml-mode
+  :straight t)
 
 (use-package flycheck
   :straight t
@@ -95,6 +153,15 @@
   :straight t
   :config (global-set-key (kbd "C-M-w") 'er/expand-region))
 
+;;(use-package markdown-mode
+;;  :straight t)
+;;
+;;(use-package markdown-toc
+;;  :straight t)
+
+(use-package haml-mode
+  :straight t)
+
 (use-package multiple-cursors
   :straight t
   :config
@@ -102,23 +169,44 @@
   (global-set-key (kbd "M-p") 'mc/mark-previous-like-this)
   (global-set-key (kbd "M-a") 'mc/mark-all-like-this))
 
-(use-package magit-org-todos
+(use-package yasnippet
   :straight t
-  :config (magit-org-todos-autoinsert))
+  :config (yas-global-mode 1))
+(use-package yasnippet-snippets
+  :straight t)
+(use-package emmet-mode
+  :straight t
+  :config
+  (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+  (add-hook 'css-mode-hook 'emmet-mode))
+
+(use-package org-pomodoro
+  :straight t)
 
 (use-package plantuml-mode
   :straight t
   :config (plantuml-set-output-type "utxt"))
-
-(use-package json-mode
+(use-package htmlize
   :straight t)
-
-(use-package git-timemachine
+(use-package groovy-mode
+  :straight t
+  :config (add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode)))
+(use-package lua-mode
   :straight t)
+;;(use-package json-mode
+;;:straight t)
+
+;;(use-package highlight-symbol
+;;  :straight t)
+;;(use-package git-timemachine
+;;:straight t)
 
 (use-package diff-hl
   :straight t
   :config (global-diff-hl-mode 1))
+
+(use-package ztree
+  :straight t)
 
 (use-package ecukes
   :straight t)
@@ -141,6 +229,94 @@
 
 (use-package goto-chg
   :straight t)
+
+;;(use-package workgroups2
+;;  :straight t
+;;  :config (workgroups-mode 1))
+
+(use-package buffer-move
+  :straight t)
+
+(use-package which-key
+  :straight t
+  :config (which-key-mode 1))
+
+(use-package deadgrep
+  :straight t)
+
+(use-package counsel
+  :straight t)
+
+(use-package swiper-helm
+  :straight t)
+(use-package command-log-mode
+  :straight t)
+
+(use-package dts-mode
+  :straight t)
+
+(use-package ggtags
+  :straight t)
+
+(use-package fzf
+  :straight t)
+(require 'hideshow)
+(require 'sgml-mode)
+(require 'nxml-mode)
+
+(add-to-list 'hs-special-modes-alist
+  '(nxml-mode
+     "<!--\\|<[^/>]*[^/]>"
+     "-->\\|</[^/>]*[^/]>"
+
+     "<!--"
+     sgml-skip-tag-forward
+     nil))
+
+(add-hook 'nxml-mode-hook 'hs-minor-mode)
+
+;; optional key bindings, easier than hs defaults
+(define-key nxml-mode-map (kbd "C-c h") 'hs-toggle-hiding)
+
+(defun my-i-search ()
+  ""
+  (interactive)
+  (if (use-region-p)
+    (let ((query (buffer-substring-no-properties (region-beginning) (region-end))))
+      (message "searching for %s" query)
+      (isearch-mode t)
+      (isearch-yank-string query))
+    (isearch-forward)))
+
+(defun xah-search-current-word ()
+  "Call `isearch' on current word or text selection.
+“word” here is A to Z, a to z, and hyphen 「-」 and underline 「_」, independent of syntax table.
+URL `http://ergoemacs.org/emacs/modernization_isearch.html'
+Version 2015-04-09"
+  (interactive)
+  (let ( $p1 $p2 )
+    (if (use-region-p)
+      (progn
+        (setq $p1 (region-beginning))
+        (setq $p2 (region-end)))
+      (save-excursion
+        (skip-chars-backward "-_A-Za-z0-9")
+        (setq $p1 (point))
+        (right-char)
+        (skip-chars-forward "-_A-Za-z0-9")
+        (setq $p2 (point))))
+    (setq mark-active nil)
+    (when (< $p1 (point))
+      (goto-char $p1))
+    (isearch-mode t)
+    (isearch-yank-string (buffer-substring-no-properties $p1 $p2))))
+
+
+;;(pending-delete-mode +1)
+
+;; setup the calendar before loading its customize variables
+(copy-face font-lock-constant-face 'calendar-iso-week-face)
+(set-face-attribute 'calendar-iso-week-face nil :height 1.0 :foreground "salmon")
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
@@ -167,12 +343,13 @@ point reaches the beginning or end of the buffer, stop there."
 
 (global-set-key [remap move-beginning-of-line] 'smarter-move-beginning-of-line)
 
-(if (< (length command-line-args) 2)
-  (helm-recentf))
+;;(if (< (length command-line-args) 2)
+;;  (helm-recentf))
+
+(use-package gcalcli-mode
+  :straight (gcalcli-mode :type git :host github :repo "gizmomogwai/gcalcli-mode"))
 
 (provide 'init)
-
-;;; init.el ends here
 
 ;;(use-package avy
 ;;  :ensure t)
@@ -205,3 +382,25 @@ point reaches the beginning or end of the buffer, stop there."
 ;;(add-hook 'projectile-before-switch-project-hook #'my-before-switch-project-hook)
 ;;(add-hook 'projectile-after-switch-project-hook #'my-after-switch-project-hook)
 
+(with-eval-after-load 'org
+  (org-babel-do-load-languages 'org-babel-load-languages
+    '(
+       (ruby . t)
+       (plantuml . t))))
+
+(defun reload-init ()
+  "Reload init.el."
+  (interactive)
+  (straight-transaction
+    (straight-mark-transaction-as-init)
+    (message "Reloading init.el...")
+    (load user-init-file nil 'nomessage)
+    (message "Reloading init.el... done.")))
+
+;; https://stackoverflow.com/a/20788581
+(require 'ansi-color)
+(add-hook 'compilation-filter-hook
+  (lambda() (when (eq major-mode 'compilation-mode)
+              (ansi-color-apply-on-region compilation-filter-start (point-max)))))
+
+;;; init.el ends here
